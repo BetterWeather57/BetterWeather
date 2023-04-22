46904b9c6c670c112fc34d1492c76ed7c9a96595
Leonard <leonard@Leonards-MacBook-Pro-2.local>
2023-04-22 13:52:36 -0700

    testing

--- src/components/SavedLocation.js
+++ src/components/SavedLocation.js
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from 'react';
 import { App } from '../App'
 
-export const SavedLocation = ({SavedLocation}) => {
+// pass in savedLocation and LocationSelect function
+export const SavedLocation = ({savedLocation, seletedLocation }) => {
 
 
   return (
     <div className='saved-location-container'>
         <div className='saved-location-cards'>
-        {SavedLocation.map((location, index) => (
-            <>
+        {savedLocation.map((location, index) => (
+            <div key={index} onClick={() => onSelect(location)} >
             <div>{location.name}</div>
             <div>{location.current_temp}</div>
             <div>{location.high_temp}</div>
             <div>{location.low_temp}</div>
-            </>
+            </div>
         ))}
         </div>
     </div>
