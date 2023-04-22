6b5097e4766ba589dadb8f0425ad9f4347ffe313
Brandon Yoon <122177926+brand0n-y00n@users.noreply.github.com>
2023-04-22 10:52:36 -0700

    testing changed App.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
 export default function App() {
+  //prop drill to SearchBar component
+  const [locationData, setLocationData] = useState('');
+  //prop drill to savedLocation -> default state array/obj?
+  const [savedLocation, setSavedLocation] = useState();
+  
+  //function to handle onSubmit
+
   return (
     <div>
-      
+      <div className ='search-bar-saved-location-container'>
+        <SearchBar />
+        <SavedLocation />
+      </div>
+
+      <div className = 'weather-stats-eco-tips-container'>
+        <WeatherStats />
+        <EcoTips />
+      </div>
     </div>
   )
 }
