ddc8ebbf2b11da69fd95474a8c10dcd3907fe18d
Ying-An <yingan.wang@berkeley.edu>
2023-04-22 13:32:38 -0700

    weather api route set up, waiting to decide what data to parse

--- server/server.js
+++ server/server.js
@@ -1,8 +1,51 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const PORT = 3000;
+
+// require routes
+
+
+
+// parse request body
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+// app.use(cookieParser()); // for req.cookies
+
+
+// require routers
+// const userRouter = require('./routes/users');
+const weatherController = require('./controllers/weatherController.js')
+
+
+// static files
+
+
+
+
+// route handlers
+// user route, saved locations
+// app.use('/', userRouter)
+
+
+// weather api route, specific location at /weather/:id ?
+app.get('/weather/:location', weatherController.getWeather, (req, res) => {
+  return res.status(200).json(res.locals.stats);
+});
+
+
+// database/saved locations route
+// app.use('/saved-locations', )
+
+
+
+
+
+
+
+
+
+
 
 //local error handler
 app.use((req, res) => res.status(404).send('This is not the page you\'re looking for...'));
