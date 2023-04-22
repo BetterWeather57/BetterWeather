f3276f91fe28dd1ee416447148e19b7827e18e78
Brandon Yoon <122177926+brand0n-y00n@users.noreply.github.com>
2023-04-22 11:25:02 -0700

    added searchedLocation into App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,26 @@ import React from 'react'
 export default function App() {
   //prop drill to SearchBar component
   const [locationData, setLocationData] = useState('');
+  const [weatherData, setWeatherData] = useState([])
   //prop drill to savedLocation -> default state array/obj?
   const [savedLocation, setSavedLocation] = useState();
   
+   async function searchLocation(e) {
+    // useState react hook for searchInput
+    let response = await fetch('/url-endpoint') //response received as Object
+    const weatherInfo = await response.json();
+    const infoArray = [...weatherInfo];
+    //pass weatherData into setWeatherData
+    await setWeatherData(infoArray);
+  }
+
+  //query from database
+  useEffect
+
   //function to handle onSubmit
+
+
+  
 
   return (
     <div>
@@ -16,7 +32,7 @@ export default function App() {
       </div>
 
       <div className = 'weather-stats-eco-tips-container'>
-        <WeatherStats />
+        <WeatherStats weatherData={weatherData} />
         <EcoTips />
       </div>
     </div>
