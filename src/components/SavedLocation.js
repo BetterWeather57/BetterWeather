bfbafa29e4d18b8fe94e495e66dc39d58dffc2fe
Brandon Yoon <122177926+brand0n-y00n@users.noreply.github.com>
2023-04-24 16:31:07 -0700

    commiting changes

--- src/components/SavedLocation.js
+++ src/components/SavedLocation.js
@@ -1,20 +1,27 @@
 import React, {useState} from 'react';
 import '../stylesheets/SavedLocations.css'
 
-
-export const SavedLocation = ({setSavedLocation, savedLocation, onSelect, selectedLocation }) => {
+export const SavedLocation = ({setSaved, setSavedLocation, savedLocation, onSelect, selectedLocation }) => {
   // console.log(testArr)
-
+  // console.log('SAVED LOCATION', savedLocation);
+  // console.log('CURRENT LOCATION', selectedLocation);
   
-  function deleteSavedLocation(name){
+  async function deleteSavedLocation(name){
     console.log('clicked delete button')
     console.log('savedLocation: ', savedLocation)
     //fetch request to backend to delete current savedLocation from database
+    // await fetch(`http://localhost:3000/user/6444bea26f5f28963025d4d5/deleteLocation`, {
+    //   method: 'DELETE',
+    //   headers: {'content-type': 'application/json'},
+    //   body: JSON.stringify({
+    //     location: `${name}`
+    //   })
+    // })
+
     let test = savedLocation;
-    test = test.filter((location)=>{
+    test = savedLocation.filter((location)=>{
       return location.location.name !== name;
     })
-    console.log('test: ', test)
     setSavedLocation(test);
     setSaved(false);
   }
@@ -24,16 +31,23 @@ export const SavedLocation = ({setSavedLocation, savedLocation, onSelect, select
     onSelect(location)
   }
 
+  //fetch request to get names of savedLocation array
+  const savedLocationList = fetch(`http://localhost:3000/user/6444bea26f5f28963025d4d5/saved`)
+  .then(response=>response.json())
+  .then(data => {
+    console.log('data from intial fetch request to get list of savedLocation: ', data)
+    return data;
+  })
+  
+
   return (
     <div className='saved-location-container'>
-      {savedLocation.map((location, index) => (
-        <div id={index} className='saved-location-cards' key={index} onClick={() => onSelectLocation(location)} style={{cursor:'pointer'}}>
-          <div>{location.location.name}, {location.location.region}</div>
-          <div>Current: {location.current.temp_f}&deg;F</div>
-          <img src={location.condition.icon}/>
-          <button id={`button${index}`} onClick={()=>{deleteSavedLocation(location.location.name)}} >delete</button>
-        </div>
-
+        {savedLocation.map((location, index) => (
+              <div id={index} className='saved-location-cards' key={index} onClick={() => onSelectLocation(location)} >
+              <div>{location.location.name}, {location.location.region}</div>
+              <div>Current: {location.current.temp_f}&deg;F</div>
+              <button id={`button${index}`} onClick={()=>{deleteSavedLocation(location.location.name)}} >delete</button>
+              </div>
         ))}
         
         </div>
